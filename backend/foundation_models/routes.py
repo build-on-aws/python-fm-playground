d943b980ac823ac28fb2a06e9773dbcca41b1481
Dennis Traub <dennis.traub@gmail.com>
2023-11-13 13:21:28 +0100

    Add an example policy and improve the readme (#9)
    Co-authored-by: Dennis Traub <traubd@amazon.com>

--- backend/foundation_models/routes.py
+++ backend/foundation_models/routes.py
@@ -1,12 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from botocore.exceptions import ClientError
 from . import service
 
 router = APIRouter()
 
 @router.get("/foundation-models")
 def list_foundation_models():
-    return service.list_foundation_models()
+    try:
+        return service.list_foundation_models()
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "AccessDeniedException":
+            raise HTTPException(status_code=403)
+        else:
+            raise HTTPException(status_code=500)
 
 @router.get("/foundation-models/model/{model_id}")
 def get_foundation_model_details(model_id: str):
-    return service.get_foundation_model(model_id)
+    try:
+        return service.get_foundation_model(model_id)
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "AccessDeniedException":
+            raise HTTPException(status_code=403)
+        else:
+            raise HTTPException(status_code=500)
+
--- backend/text_playground/routes.py
+++ backend/text_playground/routes.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from botocore.exceptions import ClientError
 from . import models
 from . import claude
 from . import jurassic2
@@ -8,11 +9,17 @@ router = APIRouter()
 
 @router.post("/foundation-models/model/text/{modelId}/invoke")
 def invoke(body: models.TextRequest, modelId: str):
-    if modelId == "anthropic.claude-v2":
-        completion = claude.invoke(body.prompt, body.temperature, body.maxTokens)
-    elif modelId == "ai21.j2-mid-v1": 
-        completion = jurassic2.invoke(body.prompt, body.temperature, body.maxTokens)
+    try:
+        if modelId == "anthropic.claude-v2":
+            completion = claude.invoke(body.prompt, body.temperature, body.maxTokens)
+        elif modelId == "ai21.j2-mid-v1":
+            completion = jurassic2.invoke(body.prompt, body.temperature, body.maxTokens)
 
-    return models.TextResponse(
-        completion=completion
-    )
+        return models.TextResponse(
+            completion=completion
+        )
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "AccessDeniedException":
+            raise HTTPException(status_code=403)
+        else:
+            raise HTTPException(status_code=500)
