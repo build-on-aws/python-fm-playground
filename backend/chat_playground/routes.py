821291dd181ad54db16c230edb27fce0f10e04e3
Dennis Traub <traubd@amazon.com>
2023-11-13 12:50:03 +0100

    Add error handling to routes

--- backend/chat_playground/routes.py
+++ backend/chat_playground/routes.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from botocore.exceptions import ClientError
 from . import models
 from . import services
 
@@ -7,8 +8,13 @@ router = APIRouter()
 
 @router.post("/foundation-models/model/chat/anthropic.claude-v2/invoke")
 def invoke(body: models.ChatRequest):
-    completion = services.invoke(body.prompt)
-    
-    return models.ChatResponse(
-        completion=completion
-    )
+    try:
+        completion = services.invoke(body.prompt)
+        return models.ChatResponse(
+            completion=completion
+        )
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "AccessDeniedException":
+            raise HTTPException(status_code=403)
+        else:
+            raise HTTPException(status_code=500)
--- backend/image_playground/routes.py
+++ backend/image_playground/routes.py
@@ -1,17 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from botocore.exceptions import ClientError
 from . import models
 from . import services
-
 
 router = APIRouter()
 
 
 @router.post("/foundation-models/model/image/stability.stable-diffusion-xl/invoke")
 def invoke(body: models.ImageRequest):
-    response = services.invoke(body.prompt, body.stylePreset)
-
-    # return models.ImageResponse(
-    #     imageByteArray=response
-    # )
-
-    return { "imageByteArray": response }
+    try:
+        response = services.invoke(body.prompt, body.stylePreset)
+        return {
+            "imageByteArray": response
+        }
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "AccessDeniedException":
+            raise HTTPException(status_code=403)
+        else:
+            raise HTTPException(status_code=500)
